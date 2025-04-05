33761b3d93fe64d7c30e2066d1ae957d3cc4fc5f
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2025-04-04 20:45:13 -0400

    Initial commit

---
+++ apps/tshirt-designer-frontend-e2e/cypress.config.ts
@@ -0,0 +1,17 @@
+import { nxE2EPreset } from '@nx/cypress/plugins/cypress-preset';
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  e2e: {
+    ...nxE2EPreset(__filename, {
+      cypressDir: 'src',
+      webServerCommands: {
+        default: 'npx nx run tshirt-designer-frontend:serve',
+        production: 'npx nx run tshirt-designer-frontend:serve-static',
+      },
+      ciWebServerCommand: 'npx nx run tshirt-designer-frontend:serve-static',
+      ciBaseUrl: 'http://localhost:4200',
+    }),
+    baseUrl: 'http://localhost:4200',
+  },
+});
