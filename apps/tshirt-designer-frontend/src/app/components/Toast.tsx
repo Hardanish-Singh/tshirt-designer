b457643717eb51fd9fb2eca9cbdb42bdccdf2ddf
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2025-04-12 19:22:45 -0400

    Tshirt Designer

--- apps/tshirt-designer-frontend/src/app/components/Toast.tsx
+++ apps/tshirt-designer-frontend/src/app/components/Toast.tsx
@@ -5,24 +5,22 @@ const Toast = ({ message, type, onClose }: ToastProps) => {
   useEffect(() => {
     const timer = setTimeout(() => {
       onClose();
-    }, 1000);
+    }, 500);
     return () => clearTimeout(timer);
   }, [message, onClose]);
+
   return (
     <div
-      style={{
-        position: 'fixed',
-        top: '20px',
-        right: '20px',
-        backgroundColor: type === 'success' ? '#4CAF50' : '#f44336',
-        color: 'white',
-        padding: '10px 20px',
-        borderRadius: '5px',
-        boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)',
-        zIndex: '1000',
-        opacity: 1,
-        transition: 'opacity 0.5s ease-out',
-      }}
+      className={`
+      fixed top-5 right-5 
+      px-5 py-2 
+      rounded 
+      text-white 
+      shadow-lg 
+      z-[1000] 
+      transition-opacity duration-500 
+      ${type === 'success' ? 'bg-green-500' : 'bg-red-500'}
+    `}
     >
       {message}
     </div>
