3fcd56648434718df323d009f1f65f5d4061f59b
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2025-07-16 09:18:19 -0400

    Tshirt Designer

--- apps/tshirt-designer-backend/src/routes/exchangeRatesRoute.ts
+++ apps/tshirt-designer-backend/src/routes/exchangeRatesRoute.ts
@@ -18,6 +18,7 @@ router.get('/', async (req: Request, res: Response) => {
   } catch (error) {
     // console.error('Error fetching data:', error);
     // res.status(500).send({ error: 'Error fetching data' });
+
     // For now will send 200 even if error is thrown, if API_KEY expires
     res.status(200).send({
       CAD: 1.559263,
