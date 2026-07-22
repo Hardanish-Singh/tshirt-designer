df7eca47c0b56dd3d79baa2bf19db76f66916d5a
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2026-07-22 11:38:44 -0400

    Tshirt Designer

--- jest.config.ts
+++ jest.config.ts
@@ -2,6 +2,7 @@ import { getJestProjectsAsync } from '@nx/jest';
 import type { Config } from 'jest';
 
 // Jest configuration for the entire monorepo
+
 export default async (): Promise<Config> => ({
   projects: await getJestProjectsAsync(),
 });
